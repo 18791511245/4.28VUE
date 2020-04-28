2596f568f41124a3e8a02a82c320fabd19f013d1
your Name <you@example.com>
2020-04-28 12:14:58 +0800

    4.28vue first commit

---
+++ vuethree/src/components/page.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       <h3>
+           <font color="blue">{{info}}</font>
+       </h3>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            info:"将进酒"
+        }
+    }
+}
+</script>
---
+++ vuethree/src/components/work.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        杯莫停
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
